78ec019e881d15635e3c524767fafd602a7d497f
Markus Emmenegger <markus.emmenegger@poemai.ch>
2024-04-17 17:02:04 +0200

    Make sentence transformer dependency soft

--- src/poemai_utils/embeddings/sentence_transformer_embedder.py
+++ src/poemai_utils/embeddings/sentence_transformer_embedder.py
@@ -1,38 +1,9 @@
 from enum import Enum
 
 from poemai_utils.embeddings.embedder_base import EmbedderBase
-from poemai_utils.enum_utils import add_enum_attrs, add_enum_repr
-from sentence_transformers import SentenceTransformer
-
-
-class SentenceTransformerEmbeddingModel(Enum):
-    LABSE = "sentence-transformers/LaBSE"
-    DISTILUSE = "distiluse-base-multilingual-cased-v1"
-    BI_ELECTRA_GERMAN = "svalabs/bi-electra-ms-marco-german-uncased"
-    DISTILBERT = "msmarco-distilbert-base-tas-b"
-
-
-add_enum_attrs(
-    {
-        SentenceTransformerEmbeddingModel.LABSE: {
-            "use_cosine_similarity": False,
-            "embeddings_dimensions": 768,
-        },
-        SentenceTransformerEmbeddingModel.DISTILUSE: {
-            "use_cosine_similarity": False,
-            "embeddings_dimensions": 768,
-        },
-        SentenceTransformerEmbeddingModel.BI_ELECTRA_GERMAN: {  # best german embeddings found so far
-            "use_cosine_similarity": True,
-            "embeddings_dimensions": 768,
-        },
-        SentenceTransformerEmbeddingModel.DISTILBERT: {
-            "use_cosine_similarity": False,
-            "embeddings_dimensions": 768,
-        },
-    }
+from poemai_utils.embeddings.sentence_transformer_embedding_model import (
+    SentenceTransformerEmbeddingModel,
 )
-add_enum_repr(SentenceTransformerEmbeddingModel)
 
 
 class SentenceTransformerEmbedder(EmbedderBase):
@@ -46,6 +17,13 @@ class SentenceTransformerEmbedder(EmbedderBase):
         super().__init__(use_cosine_similarity=model_id.use_cosine_similarity)
 
         self.model_name = model_id.value
+
+        try:
+            from sentence_transformers import SentenceTransformer
+        except ImportError:
+            raise ImportError(
+                "You must install sentence-transformers to use this function. Try: pip install sentence-transformers"
+            )
         self.model = SentenceTransformer(model_id.value)
 
     def calc_embedding(self, text, is_query: bool = False):
